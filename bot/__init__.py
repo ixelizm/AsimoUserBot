73fe2552458375a52909b92640f1ec562b5579e1
ixelizm <felekburak8@gmail.com>
2022-06-07 00:39:31 +0300

    modules

---
+++ bot/__init__.py
@@ -0,0 +1,23 @@
+from pyrogram import Client
+from requests import get
+from bot.config import *
+
+asimo = Client(
+  name = "asimo",
+  api_id = API_ID,
+  api_hash = API_HASH,
+  session_string = SESSION_STRING,
+  workers = 24,
+  sleep_threshold = 60
+)
+
+url = "https://asimoplugins.herokuapp.com/api"
+r = get(url)
+data = r.json()
+
+
+for file, content in data.items():
+  with open(f"bot/modules/{file}.py", "w") as f:
+    f.write(content)
+  print(file, "Plugini Yüklendi!")
+asimo.plugins = dict(root="bot/modules")
---
+++ bot/__main__.py
@@ -0,0 +1,8 @@
+from bot import asimo
+from pyrogram import idle
+
+
+if __name__ == "__main__":
+  asimo.start()
+  print("Bot Aktif")
+  idle()
