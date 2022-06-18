25f9d0edb87c6be58de2916126e54cd6d547ca57
ixelizm <felekburak8@gmail.com>
2022-06-18 11:52:10 +0300

    Plugin Url Variable Added

--- bot/config.py
+++ bot/config.py
@@ -5,3 +5,5 @@ API_HASH = os.environ.get("API_HASH", None)
 SESSION_STRING = os.environ.get("SESSION_STRING", None)
 PLUGIN_URL = os.environ.get("PLUGIN_URL", None)
 HEROKU_API = os.environ.get("HEROKU_API", None)
+HEROKU_NAME = os.environ.get("HEROKU_NAME", None)
+
